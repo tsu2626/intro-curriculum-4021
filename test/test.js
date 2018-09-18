48229a5b26854af7639f4efe2b462631a9378046
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-09-18 14:36:44 +0000

    create delete test

--- test/test.js
+++ test/test.js
@@ -268,19 +268,23 @@ describe('/schedules/:scheduleId?delete=1', () => {
               where: { scheduleId: scheduleId }
             }).then((comments) => {
               // TODO テストを実装
+              assert.equal(comments.length, 0);
             });
             const p2 = Availability.findAll({
               where: { scheduleId: scheduleId }
             }).then((availabilities) => {
               // TODO テストを実装
+              assert.equal(availabilities.length, 0);
             });
             const p3 = Candidate.findAll({
               where: { scheduleId: scheduleId }
             }).then((candidates) => {
               // TODO テストを実装
+              assert.equal(candidates.length, 0);
             });
             const p4 = Schedule.findById(scheduleId).then((schedule) => {
               // TODO テストを実装
+              assert.equal(!schedule, true);
             });
             Promise.all([p1, p2, p3, p4]).then(() => {
               if (err) return done(err);
